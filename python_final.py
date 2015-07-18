547a88eaced0b6dcdf451f182b38ebc078caa631
minhappylee <minhappylee@users.noreply.github.com>
2015-07-19 01:04:03 +0200

    Update python_final.py

--- python_final.py
+++ python_final.py
@@ -6,25 +6,6 @@ import random
 class growth():
 
 
-#Growth is a multi-functional modeling tool.
-#Its versatility extends for individual geometry and also for edges that they belong to.
-#The x,y,z, angles and scales are changeable, but at least one edge and one angle should be selected.
-#It is automatically set to randomize the entirety of the geometry with every click of the button "grow".
-#But, the user can select any edge of the geometry and adjust its extrusion values. 
-#Also, if one selects a lone edge and click "grow", the edge itself becomes a changing line that can be manipulated.
-#The user can then opt to click on the entire geometry and click "grow" again to extrude the faces out 
-#of the line that is made out of various edges. 
-#In summary, Growth has three main functions:
-#1) controlled geometry extrusion based on x,y,z choices
-#2) randomized geometry extrusion after the first click of "grow"
-#3) controlled and randomized extrusion of the edges of a given geometry. 
-
-#Created by Minha Lee 
-#Last modified by Minha Lee on 05/09/10
-#0minhalee0@gmail.com
-
-
-
 	def __init__(self):
 		self.name="growthUI"
 		self.title="growth"
